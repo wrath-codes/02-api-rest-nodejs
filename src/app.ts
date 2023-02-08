f01f16fc2bb6e85293ebe3d8349b07827ef85756
wrathCodes <rvazn.ds@gmail.com>
2023-02-08 12:13:01 -0300

    done testing

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import cookie from '@fastify/cookie'
+import fastify from 'fastify'
+import { transactionRoutes } from './routes/transactions'
+
+export const app = fastify()
+app.register(cookie)
+
+app.register(transactionRoutes, { prefix: '/transactions' })
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,5 @@
-import cookie from '@fastify/cookie'
-import fastify from 'fastify'
 import { env } from './env'
-import { transactionRoutes } from './routes/transactions'
-
-const app = fastify()
-app.register(cookie)
-
-app.register(transactionRoutes, { prefix: '/transactions' })
+import { app } from './app'
 
 app
   .listen({
